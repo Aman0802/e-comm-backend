4f1dba1bed585972b26bf403be1013839ea52249
Aman0802 <aman.khubani@gmail.com>
2021-10-24 00:57:14 +0530

    added env variable for password

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 module.exports = {
   development: {
     username: "postgres",
-    password: "postgres",
+    password: process.env.DB_PASSWORD,
     database: "e-commerce-minor",
     host: "127.0.0.1",
     dialect: "postgres",
