6f9bd7e8cdd96c8279cad19197fdd663587a882a
omBhatnagar <omishb2001@gmail.com>
2021-10-28 16:12:49 +0530

    IU-Relations-Relationship started.

--- src/database/models/productimage.js
+++ src/database/models/productimage.js
@@ -9,6 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      // Ima need these n̶u̶d̶e̶s̶  images to link to products to start the relationship ;)
     }
   }
   ProductImage.init(
