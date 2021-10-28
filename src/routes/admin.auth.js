b0df8952b34eee869612260aa662437349e1e19b
Aman0802 <aman.khubani@gmail.com>
2021-10-28 19:32:46 +0530

    SU - setup the routes

---
+++ src/routes/admin.auth.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+
+// const {} = require("../controllers/admin.controller");
+
+router.post("/products", () => {});
+
+router.delete("/products/:productId", () => {});
+
+router.put("/products/:productId", () => {});
+
+router.delete("/reviews/:reviewId", () => {});
+
+router.post("/answer-question/:questionId", () => {});
+
+module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,18 @@
 const router = require("express").Router();
 
 const authRoutes = require("./auth.route");
+// const adminRoutes = require("./admin.route");
+// const userRoutes = require("./user.route");
 
 router.use("/auth", authRoutes);
+// router.use("/admin", adminRoutes);
+// router.use("/user", userRoutes);
+
+// Getting Products accessible to both user and admin
+router.get("/products", () => {});
+
+router.get("/products?productId=:productId", () => {});
+
+router.get("/products?categoryId=:categoryId", () => {});
 
 module.exports = router;
