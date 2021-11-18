f3da2eedcc738937a7267b242a5705088123a15a
omBhatnagar <omishb2001@gmail.com>
2021-11-18 17:21:45 +0530

    IU-Added auth error handling.

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,64 +1,87 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const { DatabaseError } = require("sequelize");
 
 const { User } = require("../database/models");
 
 exports.loginController = async (req, res, next) => {
-  const { userEmail, password } = req.body;
+	const { userEmail, password } = req.body;
 
-  try {
-    const user = await User.findByPk(userEmail);
-    if (user) {
-      // validate password
-      const validPassword = await user.validatePassword(password);
-      if (validPassword) {
-        const token = await jwt.sign(
-          {
-            email: user.email,
-            role: user.role,
-          },
-          process.env.USER_JWT_SECRET
-        );
+	try {
+		if (!userEmail || !password) {
+			throw new Error("Email or password cannot be empty.");
+		}
 
-        res.status(200).send({
-          code: 200,
-          status: true,
-          message: "Logged in Successfully",
-          data: {
-            accessToken: token,
-          },
-        });
-      }
-    } else {
-      res.status(400).send({
-        code: 400,
-        status: true,
-        message: "User not found!",
-      });
-    }
-  } catch (err) {
-    console.log(err);
-  }
+		const user = await User.findByPk(userEmail);
+		if (user) {
+			// validate password
+			const validPassword = await user.validatePassword(password);
+			if (validPassword) {
+				const token = await jwt.sign(
+					{
+						email: user.email,
+						role: user.role,
+					},
+					process.env.USER_JWT_SECRET
+				);
+
+				res.status(200).send({
+					code: 200,
+					status: true,
+					message: "Logged in Successfully",
+					data: {
+						accessToken: token,
+					},
+				});
+			} else {
+				throw new Error("Email or password is incorrect.");
+			}
+		} else {
+			throw new Error("User does not exists.");
+		}
+	} catch (err) {
+		if (err instanceof DatabaseError) {
+			const error = new Error(err);
+			error.httpStatusCode = 500;
+			return next(error);
+		} else {
+			const error = new Error(err);
+			error.httpStatusCode = 400;
+			return next(error);
+		}
+	}
 };
 
 exports.registerController = async (req, res, next) => {
-  const { userEmail, password, role } = req.body;
+	const { userEmail, password, role } = req.body;
 
-  try {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    console.log("hashed password", hashedPassword);
-    await User.create({
-      email: userEmail,
-      password: hashedPassword,
-      role,
-    });
+	try {
+		if (!userEmail || !password) {
+			throw new Error("Email or password cannot be empty.");
+		}
 
-    res.status(200).send({
-      code: 200,
-      status: true,
-      message: "User created Successfully",
-    });
-  } catch (err) {
-    console.log(err);
-  }
+		const hashedPassword = await bcrypt.hash(password, 10);
+		console.log("hashed password", hashedPassword);
+		await User.create({
+			email: userEmail,
+			password: hashedPassword,
+			role,
+		});
+
+		res.status(200).send({
+			code: 200,
+			status: true,
+			message: "User created Successfully",
+		});
+	} catch (err) {
+		if (err instanceof DatabaseError) {
+			const error = new Error(err);
+			error.httpStatusCode = 500;
+			return next(error);
+		} else {
+			const error = new Error(err);
+			error.httpStatusCode = 400;
+			return next(error);
+		}
+	}
 };
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-const passport = require('passport');
+const passport = require("passport");
 
 dotenv.config();
 
@@ -12,10 +12,10 @@ const routes = require("./routes");
 
 // Disable cross origin resource sharing when development was done.
 app.use(
-  cors({
-    origin: true,
-    credentials: true,
-  })
+	cors({
+		origin: true,
+		credentials: true,
+	})
 );
 
 // Middlewares
@@ -25,11 +25,13 @@ app.use(passport.initialize());
 
 // Routes
 app.use("/api", routes);
+
+// Error handling middleware
 app.use((error, req, res, next) => {
-  return res.status(error.httpStatusCode).send({
-    code: error.httpStatusCode,
-    message: error.message
-  });
+	return res.status(error.httpStatusCode).send({
+		code: error.httpStatusCode,
+		message: error.message,
+	});
 });
 
 // Port
