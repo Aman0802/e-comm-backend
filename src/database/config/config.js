a8235c899f50710a7f6da75e6beac55e17a95ab5
aman0802 <aman.khubani@gmail.com>
2022-09-27 01:11:12 +0530

    ready to deploy

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: "postgres",
+    username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: "e-commerce-minor",
+    database: process.env.DB_DBNAME,
     host: "127.0.0.1",
     dialect: "postgres",
   },
