77f927561e2bfe8f9798c4cd6ffea13b1a8682c4
aman0802 <aman.khubani@gmail.com>
2022-09-30 21:56:28 +0530

    updated config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -12,7 +12,7 @@ module.exports = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DBNAME,
-    host: "127.0.0.1",
+    host: process.env.DB_HOST,
     dialect: "postgres",
   },
 };
